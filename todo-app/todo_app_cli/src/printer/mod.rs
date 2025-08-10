a1c098dd755a9a3d8a143705174a0f8797ecd2b3
Dhruv Mohindru <dmohindru@gmail.com>
2025-08-11 09:26:50 +1000

    Wrote tests for printer module

--- todo-app/todo_app_cli/src/printer/mod.rs
+++ todo-app/todo_app_cli/src/printer/mod.rs
@@ -28,9 +28,9 @@ impl<W: Write> TodoPrinter<W> {
 
 #[cfg(test)]
 mod tests {
-    use nanoid::nanoid;
-
     use super::*;
+    use nanoid::nanoid;
+    use serde_json::to_string_pretty;
     use std::io::Cursor;
 
     const ID_LENGTH: usize = 7;
@@ -38,8 +38,8 @@ mod tests {
     fn get_todo_single() -> Todo {
         Todo {
             id: nanoid!(ID_LENGTH),
-            name: String::from("First Todo"),
-            description: String::from("First Todo description"),
+            name: String::from("First Todo Long Title"),
+            description: String::from("First Todo very long description"),
             completed: false,
         }
     }
@@ -67,23 +67,91 @@ mod tests {
         ]
     }
 
+    fn setup() -> Cursor<Vec<u8>> {
+        Cursor::new(Vec::<u8>::new())
+    }
+
+    fn first_10_chars(s: &String) -> String {
+        s.chars().take(10).collect()
+    }
+
+    fn get_expected_text_format(todo: Todo) -> String {
+        // expected format
+        // id done title description
+        format!(
+            "{}\t{}\t{}\t{}",
+            todo.id,
+            todo.completed,
+            first_10_chars(&todo.name),
+            todo.description
+        )
+    }
+
     #[test]
     fn should_print_single_todo_in_text_format() {
-        todo!()
+        let output_cur = setup();
+        let mut printer = TodoPrinter::new(output_cur);
+        let single_todo = get_todo_single();
+        printer.print_single_todo(single_todo.clone(), OutputFormat::Text);
+
+        // Convert written data back to string
+        let output_bytes = printer.into_writer().into_inner();
+        let output_str = String::from_utf8(output_bytes).unwrap();
+
+        let expected_output = get_expected_text_format(single_todo);
+
+        assert_eq!(expected_output, output_str);
     }
 
     #[test]
     fn should_print_single_todo_in_json_format() {
-        todo!()
+        let output_cur = setup();
+        let mut printer = TodoPrinter::new(output_cur);
+        let single_todo = get_todo_single();
+        printer.print_single_todo(single_todo.clone(), OutputFormat::Json);
+
+        // Convert written data back to string
+        let output_bytes = printer.into_writer().into_inner();
+        let output_str = String::from_utf8(output_bytes).unwrap();
+
+        let expected_output = to_string_pretty(&single_todo).unwrap();
+
+        assert_eq!(expected_output, output_str);
     }
 
     #[test]
     fn should_print_list_of_todos_in_text_format() {
-        todo!()
+        let output_cur = setup();
+        let mut printer = TodoPrinter::new(output_cur);
+        let list_todo = get_todo_list();
+        printer.print_list_todo(list_todo.clone(), OutputFormat::Text);
+
+        // Convert written data back to string
+        let output_bytes = printer.into_writer().into_inner();
+        let output_str = String::from_utf8(output_bytes).unwrap();
+
+        let expected_output = list_todo
+            .into_iter()
+            .map(|todo| get_expected_text_format(todo))
+            .collect::<Vec<_>>()
+            .join("\n");
+
+        assert_eq!(expected_output, output_str);
     }
 
     #[test]
     fn should_print_list_of_todos_in_json_format() {
-        todo!()
+        let output_cur = setup();
+        let mut printer = TodoPrinter::new(output_cur);
+        let list_todo = get_todo_list();
+        printer.print_list_todo(list_todo.clone(), OutputFormat::Json);
+
+        // Convert written data back to string
+        let output_bytes = printer.into_writer().into_inner();
+        let output_str = String::from_utf8(output_bytes).unwrap();
+
+        let expected_output = to_string_pretty(&list_todo).unwrap();
+
+        assert_eq!(expected_output, output_str);
     }
 }
