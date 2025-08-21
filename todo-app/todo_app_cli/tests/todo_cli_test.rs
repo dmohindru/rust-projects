4ee20e5678d7879488de98e11f1f70195358c73b
Dhruv Mohindru <dmohindru@gmail.com>
2025-08-21 18:27:52 +1000

    Completed integration tests

--- todo-app/todo_app_cli/tests/todo_cli_test.rs
+++ todo-app/todo_app_cli/tests/todo_cli_test.rs
@@ -1,8 +1,8 @@
 use assert_cmd::Command;
 use nanoid::nanoid;
 use predicates::str::contains;
-use serde_json::to_string_pretty;
-use std::fs::write;
+use serde_json::{from_str, to_string_pretty};
+use std::fs::{read, write};
 use tempfile::NamedTempFile;
 use todo::todo_repo::Todo;
 
@@ -146,12 +146,62 @@ fn get_name_should_exit_with_non_zero_exit_code_when_todo_not_present() {
 
 #[test]
 fn add_todo_should_return_new_todo_with_zero_exit_code() {
-    todo!()
+    let todos = get_todo_list();
+    let tempfile = setup(&todos);
+    let path = tempfile.path().to_str().unwrap();
+    let new_todo_name = "New Todo";
+    let new_todo_description = "New Todo Description";
+
+    let mut cmd = Command::cargo_bin("todo").unwrap();
+    cmd.arg("--file")
+        .arg(path)
+        .arg("add")
+        .arg("--name")
+        .arg(new_todo_name)
+        .arg("--description")
+        .arg(new_todo_description);
+
+    cmd.assert()
+        .success()
+        .code(0)
+        .stdout(contains(new_todo_name));
+
+    let file_contents: Vec<u8> = read(path).unwrap();
+    let text_content = String::from_utf8_lossy(&file_contents);
+    let updated_todo_list: Vec<Todo> = from_str(&text_content).unwrap();
+    let result = updated_todo_list
+        .iter()
+        .any(|todo| todo.name == new_todo_name);
+
+    assert!(result);
 }
 
 #[test]
 fn complete_todo_should_mark_todo_complete_with_zero_exit_code() {
-    todo!()
+    let todos = get_todo_list();
+    let tempfile = setup(&todos);
+    let path = tempfile.path().to_str().unwrap();
+
+    let mut cmd = Command::cargo_bin("todo").unwrap();
+    cmd.arg("--file")
+        .arg(path)
+        .arg("complete")
+        .arg("--id")
+        .arg(todos[0].id.as_str());
+
+    cmd.assert()
+        .success()
+        .code(0)
+        .stdout(contains(todos[0].name.as_str()));
+
+    let file_contents: Vec<u8> = read(path).unwrap();
+    let text_content = String::from_utf8_lossy(&file_contents);
+    let updated_todo_list: Vec<Todo> = from_str(&text_content).unwrap();
+    let result = updated_todo_list
+        .iter()
+        .any(|todo| todo.id == todos[0].id && todo.completed);
+
+    assert!(result);
 }
 
 #[test]
@@ -175,7 +225,28 @@ fn complete_todo_should_exit_with_non_zero_exit_code_when_todo_not_present() {
 
 #[test]
 fn delete_todo_should_delete_todo_with_zero_exit_code() {
-    todo!()
+    let todos = get_todo_list();
+    let tempfile = setup(&todos);
+    let path = tempfile.path().to_str().unwrap();
+
+    let mut cmd = Command::cargo_bin("todo").unwrap();
+    cmd.arg("--file")
+        .arg(path)
+        .arg("delete")
+        .arg("--id")
+        .arg(todos[0].id.as_str());
+
+    cmd.assert()
+        .success()
+        .code(0)
+        .stdout(contains(todos[0].name.as_str()));
+
+    let file_contents: Vec<u8> = read(path).unwrap();
+    let text_content = String::from_utf8_lossy(&file_contents);
+    let updated_todo_list: Vec<Todo> = from_str(&text_content).unwrap();
+    let result = updated_todo_list.iter().any(|todo| todo.id == todos[0].id);
+
+    assert!(!result);
 }
 
 #[test]
