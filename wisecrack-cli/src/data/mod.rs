849f6fae7d31dd12a765cd5e30d59fea3be46ff4
Dhruv Mohindru <dmohindru@gmail.com>
2025-10-02 09:04:48 +1000

    Completed data api module

--- wisecrack-cli/src/data/mod.rs
+++ wisecrack-cli/src/data/mod.rs
@@ -1,11 +1,18 @@
+use core::fmt;
+
 use chrono::{DateTime, Utc};
-use reqwest::blocking::Client;
+use reqwest::{blocking::Client, header::ACCEPT};
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 struct DadJoke {
     joke: String,
     status: u16,
+}
+
+#[derive(Debug, Deserialize)]
+struct Quotes {
+    q: String,
 }
 
 pub struct Data {
@@ -31,63 +38,108 @@ impl Data {
     }
 }
 
-pub trait DataProvider {
-    fn fetch_dad_jokes(&self) -> Data;
-}
-
-pub struct ApiDataProvider {
-    client: Client,
-}
-
-impl ApiDataProvider {
-    pub fn new() -> Self {
-        ApiDataProvider {
-            client: Client::new(),
+impl fmt::Display for Data {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "Time: {}", self.time)?;
+        writeln!(f, "Response Code: {}", self.response_code)?;
+        match &self.data {
+            Some(d) => writeln!(f, "Data: {}", d)?,
+            None => writeln!(f, "Data: <none>")?,
         }
-    }
-}
-
-impl DataProvider for ApiDataProvider {
-    fn fetch_dad_jokes(&self) -> Data {
-        let url = "https://icanhazdadjoke.com";
-        let response = match self.client.get(url).send() {
-            Ok(res) => res,
-            Err(e) => {
-                return Data {
-                    data: None,
-                    time: Utc::now(),
-                    error_message: Some(e.to_string()),
-                    // TODO: find a better way to extract this value
-                    response_code: 500,
-                };
-            }
-        };
-
-        let joke: DadJoke = match response.json() {
-            Ok(joke) => joke,
-            Err(e) => {
-                return Data {
-                    data: None,
-                    time: Utc::now(),
-                    error_message: Some(e.to_string()),
-                    // TODO: find a better way to extract this value
-                    response_code: 500,
-                };
-            }
-        };
-        Data {
-            data: Some(joke.joke),
-            time: Utc::now(),
-            error_message: None,
-            response_code: joke.status,
+        match &self.error_message {
+            Some(e) => writeln!(f, "Error: {}", e)?,
+            None => writeln!(f, "Error: <none>")?,
         }
+        Ok(())
     }
 }
 
 pub fn fetch_dad_jokes() -> Data {
-    Data::new(Some(String::from("Some Dad joke")), Utc::now(), None, 200)
+    let client = Client::new();
+    let url = "https://icanhazdadjoke.com";
+    let response = match client.get(url).header(ACCEPT, "application/json").send() {
+        Ok(res) => res,
+        Err(e) => {
+            return Data {
+                data: None,
+                time: Utc::now(),
+                error_message: Some(e.to_string()),
+                // TODO: find a better way to extract this value
+                response_code: 500,
+            };
+        }
+    };
+
+    let joke: DadJoke = match response.json() {
+        Ok(joke) => joke,
+        Err(e) => {
+            return Data {
+                data: None,
+                time: Utc::now(),
+                error_message: Some(e.to_string()),
+                // TODO: find a better way to extract this value
+                response_code: 500,
+            };
+        }
+    };
+    Data {
+        data: Some(joke.joke),
+        time: Utc::now(),
+        error_message: None,
+        response_code: joke.status,
+    }
 }
 
 pub fn fetch_quote() -> Data {
-    Data::new(None, Utc::now(), Some(String::from("Error happened")), 403)
+    let client = Client::new();
+    let url = "https://zenquotes.io/api/random";
+    let response = match client.get(url).send() {
+        Ok(res) => res,
+        Err(e) => {
+            return Data {
+                data: None,
+                time: Utc::now(),
+                error_message: Some(e.to_string()),
+                // TODO: find a better way to extract this value
+                response_code: 500,
+            };
+        }
+    };
+    let response_status = response.status().as_u16();
+
+    let quotes: Vec<Quotes> = match response.json() {
+        Ok(quotes) => quotes,
+        Err(e) => {
+            return Data {
+                data: None,
+                time: Utc::now(),
+                error_message: Some(e.to_string()),
+                // TODO: find a better way to extract this value
+                response_code: 500,
+            };
+        }
+    };
+    let quote_text = quotes.get(0).map(|q| q.q.clone()).unwrap();
+    Data {
+        data: Some(quote_text),
+        time: Utc::now(),
+        error_message: None,
+        response_code: response_status,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_dad_joke_api() {
+        let data = fetch_dad_jokes();
+        println!("{}", data);
+    }
+
+    #[test]
+    fn test_quotes_api() {
+        let data = fetch_quote();
+        println!("{}", data);
+    }
 }
