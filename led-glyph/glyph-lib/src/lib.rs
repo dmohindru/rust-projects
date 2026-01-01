a8ec7a9ccf9159f7e792ade94a84691c64e5bce2
Dhruv Mohindru <dmohindru@gmail.com>
2026-01-02 08:34:45 +1100

    Making progress in writing library code

--- led-glyph/glyph-lib/src/lib.rs
+++ led-glyph/glyph-lib/src/lib.rs
@@ -1,17 +1,17 @@
-use std::error::Error;
+pub enum AnimationMode {
+    Next,
+    Scroll,
+}
 
-struct Glyph {
+#[derive(PartialEq, Debug)]
+pub struct Glyph {
     width: u8,
     height: u8,
-    bitmap: Vec<u16>,
+    bitmap: Vec<u8>,
 }
 
 impl Glyph {
-    pub fn new(size: u8, bitmap: Vec<u16>) -> Result<Glyph, &'static str> {
-        // Logic to implement here
-        // Glyph would be of size square
-        // Validation to run
-        // 1. Num of records in bitmap should be equal to size
+    pub fn new(size: u8, bitmap: Vec<u8>) -> Result<Glyph, &'static str> {
         if bitmap.len() != size as usize {
             return Err("Not enough row data as per size");
         }
@@ -21,6 +21,33 @@ impl Glyph {
             bitmap,
         })
     }
+
+    /// Width of the glyph
+    pub fn width(&self) -> u8 {
+        self.width
+    }
+
+    /// Height of the glyph
+    pub fn height(&self) -> u8 {
+        self.height
+    }
+
+    /// Bitmap data as slice
+    pub fn bitmap(&self) -> &[u8] {
+        &self.bitmap
+    }
+}
+
+pub fn glyph_animation_frames(glyphs: Vec<Glyph>, mode: AnimationMode) -> Vec<u8> {
+    validate_glyph_list(&glyphs);
+    todo!()
+}
+
+fn validate_glyph_list(glyphs: &Vec<Glyph>) -> bool {
+    let first_glyph = &glyphs[0];
+    glyphs
+        .iter()
+        .all(|glyph| glyph.height == first_glyph.height)
 }
 
 #[cfg(test)]
@@ -29,11 +56,17 @@ mod tests {
 
     #[test]
     fn should_return_error_if_row_data_not_equal_to_size_args() {
-        todo!()
+        let bitmap: Vec<u8> = vec![0x01, 0x02, 0x03];
+        let glyph_result = Glyph::new(4, bitmap);
+        assert_eq!(glyph_result, Err("Not enough row data as per size"));
     }
 
     #[test]
     fn should_return_glyph_object_if_row_data_equal_to_size_args() {
-        todo!()
+        let bitmap: Vec<u8> = vec![0x01, 0x02, 0x03];
+        let glyph = Glyph::new(3, bitmap.clone()).unwrap();
+        assert_eq!(glyph.width(), 3);
+        assert_eq!(glyph.height(), 3);
+        assert_eq!(glyph.bitmap(), &bitmap);
     }
 }
