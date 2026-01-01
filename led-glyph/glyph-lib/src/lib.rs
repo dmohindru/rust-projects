af8d842e3c8a439a82bd41f6b06ea8b7de190056
Dhruv Mohindru <dmohindru@gmail.com>
2026-01-01 13:21:16 +1100

    Started writing lib code

--- led-glyph/glyph-lib/src/lib.rs
+++ led-glyph/glyph-lib/src/lib.rs
@@ -1,5 +1,26 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::error::Error;
+
+struct Glyph {
+    width: u8,
+    height: u8,
+    bitmap: Vec<u16>,
+}
+
+impl Glyph {
+    pub fn new(size: u8, bitmap: Vec<u16>) -> Result<Glyph, &'static str> {
+        // Logic to implement here
+        // Glyph would be of size square
+        // Validation to run
+        // 1. Num of records in bitmap should be equal to size
+        if bitmap.len() != size as usize {
+            return Err("Not enough row data as per size");
+        }
+        Ok(Glyph {
+            width: size,
+            height: size,
+            bitmap,
+        })
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +28,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn should_return_error_if_row_data_not_equal_to_size_args() {
+        todo!()
+    }
+
+    #[test]
+    fn should_return_glyph_object_if_row_data_equal_to_size_args() {
+        todo!()
     }
 }
