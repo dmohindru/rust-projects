45b1eb31231dd51cedbaf1344fb5848eb4e701c0
Dhruv Mohindru <dmohindru@gmail.com>
2026-02-18 13:15:09 +1100

    Finished implementing scroll mode for text animation

--- led-glyph/glyph-cli/src/main.rs
+++ led-glyph/glyph-cli/src/main.rs
@@ -6,6 +6,7 @@ use crate::{
     cli::GlyphCli,
 };
 use clap::Parser;
+use std::process;
 use std::{fs::OpenOptions, io::Read};
 
 fn main() {
@@ -19,7 +20,8 @@ fn run() -> Result<(), String> {
     let cli = GlyphCli::parse();
 
     if cli.info {
-        print_data_file_info(&cli.file, cli.grid_size)?
+        print_data_file_info(&cli.file, cli.grid_size)?;
+        process::exit(0)
     }
 
     let file_data_access = FileDataAccess::new(cli.grid_size, cli.file);
@@ -40,14 +42,21 @@ fn print_data_file_info(file_path: &String, grid_size: u8) -> Result<(), String>
     let mut file = OpenOptions::new().read(true).open(file_path).unwrap();
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer).unwrap();
-    let frame_count = get_frame_count(&buffer, grid_size).unwrap();
-    println!("{}", frame_count);
-    Ok(())
+    return match get_frame_count(&buffer, grid_size) {
+        Ok(frame_count) => {
+            println!("{}", frame_count);
+            Ok(())
+        }
+        Err(message) => {
+            eprintln!("{}", message);
+            process::exit(1);
+        }
+    };
 }
 
 fn get_frame_count(buffer: &Vec<u8>, grid_size: u8) -> Result<u8, String> {
     if buffer.len() % grid_size as usize != 0 {
-        Err(String::from("Invalid frame Data"))
+        Err(String::from("Invalid frame data"))
     } else {
         let frame_count = buffer.len() as u8 / grid_size;
         Ok(frame_count)
@@ -60,9 +69,21 @@ mod tests {
 
     #[test]
     fn should_return_error_result_for_invalid_frame_data_and_grid_size() {
-        todo!()
+        let mut buffer: Vec<u8> = Vec::new();
+        for _ in 0..24 {
+            buffer.push(0x01);
+        }
+        let message = get_frame_count(&buffer, 5).unwrap_err();
+        assert_eq!(message, String::from("Invalid frame data"));
     }
 
     #[test]
-    fn should_return_ok_result_for_valid_frame_data_and_grid_size() {}
+    fn should_return_ok_result_for_valid_frame_data_and_grid_size() {
+        let mut buffer: Vec<u8> = Vec::new();
+        for _ in 0..25 {
+            buffer.push(0x01);
+        }
+        let frame_count = get_frame_count(&buffer, 5).unwrap();
+        assert_eq!(frame_count, 5);
+    }
 }
